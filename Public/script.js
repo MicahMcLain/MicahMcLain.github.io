8e9a3b2a2ada9d2072a5ee16b8f7b339d34453e3
MicahMcLain <mclainmicah1@gmail.com>
2023-11-14 19:32:15 -0500

    moved index back into root folder. messaging still not working. said there was an email/password error. double checked credentials and they were right. will try again later

--- Public/script.js
+++ Public/script.js
@@ -1,130 +1,134 @@
 // toggle nav bar
 
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 //scroll sections
-let sections = document.querySelectorAll('section');
+let sections = document.querySelectorAll("section");
 
-let navLinks = document.querySelectorAll('header nav a');
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*= ' + id + ']').classList.add('active');
-            });
-            sec.classList.add('show-animate');
-        } else {
-            sec.classList.remove('show-animate');
-        }
-    });
-    
-    let header = document.querySelector('header');
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*= " + id + "]")
+          .classList.add("active");
+      });
+      sec.classList.add("show-animate");
+    } else {
+      sec.classList.remove("show-animate");
+    }
+  });
 
-    header.classList.toggle('sticky', window.scrollY > 100);
-//removes navbar after scrolling to selected section
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-//footer animation
-    let footer = document.querySelector('footer');
-    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+  let header = document.querySelector("header");
 
-}
-
-
-
+  header.classList.toggle("sticky", window.scrollY > 100);
+  //removes navbar after scrolling to selected section
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+  //footer animation
+  let footer = document.querySelector("footer");
+  footer.classList.toggle(
+    "show-animate",
+    this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight
+  );
+};
 
 //contact form and email sending code
-const contactForm = document.querySelector('.contact-form');
+const contactForm = document.querySelector(".contact-form");
 
-let name = document.getElementById('name');
-let email = document.getElementById('email');
-let phoneNum = document.getElementById('phoneNum');
-let subject = document.getElementById('subject');
-let message = document.getElementById('message');
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let phoneNum = document.getElementById("phoneNum");
+let subject = document.getElementById("subject");
+let message = document.getElementById("message");
 
-contactForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    
-    let formData = {
-        name: name.value,
-        email: email.value,
-        phoneNum: phoneNum.value,
-        subject: subject.value,
-        message: message.value
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let formData = {
+    name: name.value,
+    email: email.value,
+    phoneNum: phoneNum.value,
+    subject: subject.value,
+    message: message.value,
+  };
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "/");
+  xhr.setRequestHeader("content-type", "application/json");
+  xhr.onload = function () {
+    console.log(xhr.responseText);
+    if (xhr.responseText == "success") {
+      alert("Email successfully sent. Thank You.");
+      name.value = "";
+      email.value = "";
+      phoneNum.value = "";
+      subject.value = "";
+      message.value = "";
+    } else {
+      alert("OOPS! Something went wrong. Please try again.");
     }
+  };
 
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', '/');
-    xhr.setRequestHeader('content-type', 'application/json');
-    xhr.onload = function(){
-        console.log(xhr.responseText);
-        if(xhr.responseText == 'success'){
-            alert('Email successfully sent. Thank You.');
-            name.value = '';
-            email.value = '';
-            phoneNum.value = '';
-            subject.value = '';
-            message.value = '';
-        }else{
-            alert('OOPS! Something went wrong. Please try again.')
-        }
-    }
+  xhr.send(JSON.stringify(formData));
+});
 
-    xhr.send(JSON.stringify(formData));
-})
-
-//contact info modal 
-document.getElementById("contactInfoButton").addEventListener("click", function () {
+//contact info modal
+document
+  .getElementById("contactInfoButton")
+  .addEventListener("click", function () {
     // Show the contact information modal
     const modal = document.getElementById("contactInfoModal");
     modal.style.display = "block";
-});
+  });
 
-document.getElementById("closeContactInfo").addEventListener("click", function () {
+document
+  .getElementById("closeContactInfo")
+  .addEventListener("click", function () {
     // Close the contact information modal
     const modal = document.getElementById("contactInfoModal");
     modal.style.display = "none";
     // modal.classList.add('modalBGAnimation');
-});
+  });
 
 // Close the modal if the user clicks outside of it
 window.addEventListener("click", function (e) {
-    const modal = document.getElementById("contactInfoModal");
-    if (e.target == modal) {
-        modal.style.display = "none";
-    }
+  const modal = document.getElementById("contactInfoModal");
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
 });
 
 //about me modal
 document.getElementById("aboutMeButton").addEventListener("click", function () {
-    const modal = document.getElementById("aboutMeModal");
-    modal.style.display = "block";
+  const modal = document.getElementById("aboutMeModal");
+  modal.style.display = "block";
 });
 
 document.getElementById("closeAboutMe").addEventListener("click", function () {
-    const modal = document.getElementById("aboutMeModal");
-    modal.style.display = "none";
+  const modal = document.getElementById("aboutMeModal");
+  modal.style.display = "none";
 });
 // Close the modal if the user clicks outside of it
 window.addEventListener("click", function (e) {
-    const modal = document.getElementById("aboutMeModal");
-    if (e.target == modal) {
-        modal.style.display = "none";
-    }
+  const modal = document.getElementById("aboutMeModal");
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
 });
-
 
 //text animation script
 
@@ -137,26 +141,26 @@ let charIndex = 0;
 let isDeleting = false;
 
 const typeEffect = () => {
-    const currentWord = words[wordIndex];
-    const currentChar = currentWord.substring(0, charIndex);
-    dynamicText.textContent = currentChar;
-    dynamicText.classList.add("stop-blinking");
+  const currentWord = words[wordIndex];
+  const currentChar = currentWord.substring(0, charIndex);
+  dynamicText.textContent = currentChar;
+  dynamicText.classList.add("stop-blinking");
 
-    if (!isDeleting && charIndex < currentWord.length) {
-        // If condition is true, type the next character
-        charIndex++;
-        setTimeout(typeEffect, 200);
-    } else if (isDeleting && charIndex > 0) {
-        // If condition is true, remove the previous character
-        charIndex--;
-        setTimeout(typeEffect, 100);
-    } else {
-        // If word is deleted then switch to the next word
-        isDeleting = !isDeleting;
-        dynamicText.classList.remove("stop-blinking");
-        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
-        setTimeout(typeEffect, 1200);
-    }
-}
+  if (!isDeleting && charIndex < currentWord.length) {
+    // If condition is true, type the next character
+    charIndex++;
+    setTimeout(typeEffect, 200);
+  } else if (isDeleting && charIndex > 0) {
+    // If condition is true, remove the previous character
+    charIndex--;
+    setTimeout(typeEffect, 100);
+  } else {
+    // If word is deleted then switch to the next word
+    isDeleting = !isDeleting;
+    dynamicText.classList.remove("stop-blinking");
+    wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+    setTimeout(typeEffect, 1200);
+  }
+};
 
 typeEffect();
--- server.js
+++ server.js
@@ -1,49 +1,47 @@
 require("dotenv").config();
 
-
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const nodemailer = require("nodemailer");
 
-const PORT = process.env || 5000;
+const PORT = process.env.PORT || 5000;
 
-
-app.use(express.static('Public'));
+app.use(express.static("Public"));
 app.use(express.json());
 
-app.get('/', (req, res)=> {
-    res.sendFile(__dirname + '/public/index.html')
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "index.html");
+});
 
-app.post('/', (req, res)=> {
-    console.log(req.body);
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASSWORD
-        }
-    })
+app.post("/", (req, res) => {
+  console.log(req.body);
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
 
-    const mailOptions = {
-        from: req.body.email,
-        to: 'mclainmicah1@gmail.com',
-        subject: `Message from ${req.body.email}: ${req.body.subject}`,
-        text: req.body.message + req.body.phoneNum
+  const mailOptions = {
+    from: req.body.email,
+    to: "mclainmicah1@gmail.com",
+    subject: `Message from ${req.body.email}: ${req.body.subject}`,
+    text: req.body.message + req.body.phoneNum,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("error");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send("success");
     }
+  });
+});
 
-    transporter.sendMail(mailOptions, (error, info) =>{
-        if(error){
-            console.log(error);
-            res.send('error');
-        }else{
-            console.log('Email sent: ' + info.response);
-            res.send('success');
-        }
-    })
-})
-
-app.listen(PORT, ()=> {
-    console.log(`server is running on ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
